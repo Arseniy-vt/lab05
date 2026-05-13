12b9fab83287b1ecffbb909be67ac32be53485da
Arseniy <aquaqew@mail.ru>
2026-05-13 16:13:08 -0500

    Add .gitignore and remove build directory

--- banking/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali1/lab05/lab05/banking/build/_deps/googletest-src/googletest
-# Build directory: /home/kali1/lab05/lab05/banking/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
